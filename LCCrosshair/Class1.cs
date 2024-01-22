7ae200d7a6c4c5577a1bb8a2f2b91726488196f0
fallo <fallo@DESKTOP-U7OGAP8>
2024-01-22 12:04:00 -0500

    Add project files.

---
+++ LCCrosshair/Class1.cs
@@ -0,0 +1,40 @@
+﻿using BepInEx;
+using BepInEx.Logging;
+using HarmonyLib;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LCCrosshair
+{
+    [BepInPlugin(modGUID, modName, modVersion)]
+    public class CrosshairBase : BaseUnityPlugin
+    {
+        private const string modGUID = "Teehee.Crosshair";
+        private const string modName = "Crosshair";
+        private const string modVersion = "1.0.0";
+
+        private readonly Harmony harmony = new Harmony(modGUID);
+
+        private static CrosshairBase Instance;
+
+        internal ManualLogSource mls;
+
+
+        void Awake()
+        {
+            if (Instance == null)
+            {
+                Instance = this;
+            }
+
+            mls = BepInEx.Logging.Logger.CreateLogSource(modGUID);
+
+            mls.LogInfo("The test mod has started >:)");
+
+            harmony.PatchAll(typeof(CrosshairBase));
+        }
+    }
+}
